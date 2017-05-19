b72a1389e57dea3a367f975240aa82f2b06808b8
Chatchawan Niyomthum <chatchawanNiyomthum@gmail.com>
2017-05-19 16:33:29 +0700

    Add files via upload

---
+++ project/angular2_practice/hero_project/src/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroesComponent }      from './heroes.component';
+import { HeroService }          from './hero.service';
+
+import { AppRoutingModule }     from './app-routing.module';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        FormsModule,
+        AppRoutingModule
+    ],
+    declarations: [
+        AppComponent,
+        HeroDetailComponent,
+        HeroesComponent,
+        DashboardComponent
+    ],
+    providers: [
+        HeroService
+    ],
+    bootstrap: [
+        AppComponent
+    ]
+})
+export class AppModule { }
